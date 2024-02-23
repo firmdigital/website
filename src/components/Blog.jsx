9587e7243e952400e9d4557f8d556fc79452bdb4
malla223 <konelamine1122@gmail.com>
2024-02-23 10:02:44 +0000

    update swiper

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -119,9 +119,9 @@ export default function Blogs() {
                 </div>
                 <a
                   href="#"
-                  className="rounded-md px-3.5 py-2 text-sm font-semibold leading-6 text-[#449ab7] ring-1 ring-inset ring-[#449ab7]/30 hover:ring-[#449ab7]/40 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#449ab7]"
+                  className="rounded-md hover:-translate-y-1 transition-all duration-300 px-3.5 py-2 text-sm font-semibold leading-6 text-[#449ab7] ring-1 ring-inset ring-[#449ab7]/30 hover:ring-[#449ab7]/40 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#449ab7]"
                 >
-                  Voir toutes nos articles{" "}
+                  Voir tous nos articles{" "}
                   <span aria-hidden="true">&rarr;</span>
                 </a>
               </div>
