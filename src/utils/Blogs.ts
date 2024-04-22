4d6e2a85f891078ca9e043a3d7f2bc6969ebc8e0
malla223 <konelamine1122@gmail.com>
2024-04-22 18:33:35 +0000

    update blog section

---
+++ src/utils/Blogs.ts
@@ -0,0 +1,30 @@
+export const blogs = [
+    {
+      title: "Visite à l'Usine Kamagaz - Une Plongée dans la Sécurité du Gaz",
+      id: 1,
+      href: "#",
+      image:
+        "../assets/blog/IMG_2844.JPG",
+      date: "23 Août 2022",
+      detail1:"Dès notre arrivée à l'usine Kamagaz, nous avons été impressionnés par l'ampleur de leurs installations. Des équipements de pointe, des procédures rigoureuses et un engagement indéfectible envers la sécurité étaient évidents à chaque étape de notre visite. Nous avons pu voir de près comment ils testent et inspectent chaque composant pour garantir des produits sûrs et fiables.",
+      detail2:"La sécurité du gaz est au cœur de notre projet SO SECURIGAZ. Au cours de notre visite, nous avons été sensibilisés à l'importance cruciale de la prévention des fuites de gaz pour protéger la vie humaine. Kamagaz nous a montré les différentes mesures de sécurité mises en place pour détecter et prévenir les fuites, ce qui nous a inspirés dans notre propre mission de sauver des vies grâce à la technologie.",
+      detail3:"Guidés par notre visite à l'usine Kamagaz, nos objectifs futurs sont clairs : continuer à améliorer notre dispositif SO SECURIGAZ en intégrant les meilleures pratiques de sécurité du gaz. Nous visons à rendre notre technologie encore plus accessible et efficace pour protéger les foyers contre les dangers du gaz.",
+      conclusion:"Notre visite à l'usine Kamagaz a été une expérience enrichissante qui a renforcé notre engagement envers la sécurité du gaz. Nous sommes reconnaissants envers Kamagaz pour leur accueil chaleureux et leur partage de connaissances précieuses. Ensemble, nous sommes déterminés à faire de chaque foyer un endroit plus sûr, un gaz à la fois.",
+      description:
+        "Lors de la première édition du concours ORANGE SUMMER CHALLENGE, nous avons eu le privilège d'être parrainés par l'usine Kamagaz, un leader dans le domaine de la sécurité du gaz. Récemment, nous avons eu l'opportunité de visiter leurs installations pour approfondir notre compréhension des mesures de sécurité du gaz, un aspect crucial pour notre projet SO SECURIGAZ.",
+    },
+    {
+      title: "Participation au concours ORANGE SUMMER CHALLENGE 1ère Edition",
+      id: 2,
+      href: "#",
+      image:
+        "../assets/blog/856059de-35b9-4b47-8b62-9eed85fe06ab.jpg",
+      date: "23 Août 2022",
+      detail1:"Une des choses les plus gratifiantes pour nous a été de voir l'enthousiasme et l'intérêt que notre projet a suscités chez les juges et le public. Cela nous a donné une validation supplémentaire de l'importance de notre mission : protéger les familles et les foyers contre les dangers du gaz.",
+      detail2:"Gagner le 1er prix d'Orange ainsi que le prix coup de cœur de Google est un honneur immense pour notre équipe. Cela nous motive à poursuivre notre travail et à faire avancer notre projet pour qu'il puisse bénéficier à un plus grand nombre de personnes.",
+      detail3:"Nous tenons à remercier chaleureusement Orange et Google pour avoir organisé ce concours inspirant et pour avoir donné aux startups comme la nôtre une plateforme pour briller. Nous sommes reconnaissants envers tous ceux qui nous ont soutenus tout au long de ce voyage, y compris nos mentors, nos partenaires et nos proches.",
+      conclusion:"Nous sommes impatients de continuer à développer SO SECURIGAZ et de faire une réelle différence dans la vie des gens. Restez à l'écoute pour les mises à jour sur notre progression !",
+      description:
+        "Participer à ce concours a été une expérience extrêmement enrichissante pour notre équipe. Nous avons eu l'opportunité de présenter notre projet devant un panel d'experts de l'industrie, des investisseurs potentiels et d'autres entrepreneurs passionnés. C'était une occasion unique de partager notre vision et de recevoir des commentaires précieux sur notre concept.",
+    },
+  ];
